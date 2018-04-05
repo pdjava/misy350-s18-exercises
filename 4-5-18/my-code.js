2fc4e5ce9fbcb385c679287755285160ad710785
Priyanka Donthi <pdonthi@udel.edu>
2018-04-05 10:38:52 -0400

    added js examples

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,19 @@
+let blist = ["Travel", "End of the semseter", "Finish the semseter strong"] ;
+console.log(blist.length);
+// "push" adding extra parameters in your arrray
+blist.push("Work");
+console.log(blist.length);
+// "pop" kicks out the list in your array
+
+/*
+blist.pop();
+blist.pop();
+console.log(blist.length);
+console.log(blist);
+*/
+
+// for loop //
+
+for(let i = 0; i<blist.length; i++) {
+  console.log(blist[i]);
+}
