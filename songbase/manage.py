0f6fa44de5cf215d033a4bab4472e0e79a4e431b
Priyanka Donthi <pdonthi@udel.edu>
2018-05-10 09:57:44 -0400

    Flask

--- songbase/manage.py
+++ songbase/manage.py
@@ -1,8 +1,16 @@
 from flask_script import Manager
-from songbase import app
+from songbase import app # db
 
 manager = Manager(app)
 
+#@manager.command
+#def deploy():
+#    db.drop_all()
+#    db.create_all()
 
 if __name__ == '__main__':
     manager.run()
+
+
+# go to command line python manage.py deploy
+# creates the database
--- songbase/songbase.py
+++ songbase/songbase.py
@@ -1,34 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
-import random
-
- # greeting_list = ['Ciao', 'Hai', 'Salut', 'Nihao']
-# print random.choice(greeting_list)
 
 
 @app.route('/')
 def index():
-    #"hello World"
-    return render_template('index.html')     # 'Hello Priyanka'
+    return render_template('index.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    # how to get form data is different for GET vs. POST
+    if request.method =="GET":
+        # return "Hello GET"
+        return render_template('form.html')
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        # return "hi, your name is %s" % first_name
+        return render_template('form.html', first_name=first_name)
 
 
-
-## dynamic content based on the page
-@app.route('/users/<string:uname>')
-def users(uname):
-    # return "hello %s" % username
-    return render_template('user.html', uname=uname)
+@app.route('/users/<string:username>')
+def users(username):
+     return render_template('user.html', uname=username)
 
 
-    @app.route('/user')
-    def user():
-        return "this is the page for users"
+@app.route('/user')
+def user():
+    return "this is the page for users"
 
-
-    #@app.route('/hello')
-    # def user():
-        # return print
 
 
 if __name__ == '__main__':
-    app.run() # debug = TRUE && host = '0.0.0.0', port = 4500
+    app.run()
